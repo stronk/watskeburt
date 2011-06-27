0242d36f198a054690cb5a1a61cf84402ba54651
stronk <koen@stronk.net>
2011-06-27 22:19:42 +0200

    Ik zeg hier heb je twintig cent, zegt-ie nee hoef je niet

--- watskeburt.php
+++ watskeburt.php
@@ -31,10 +31,10 @@ Je bent een sjembek zonder klauw en je bent niet 'bout it 'bout
 OK Vieze Freddy is aandeburt
 Vieze Freddy, alsof je dat niet wist en ik drink tot de motherfokking fles leeg is
 Heb pas doekoe als ik bierflesjes inwissel dan ben ik pas stang yo uhh, fo' shizzle
-Je lacht maar ik maak hier geen motherfucking grappen
+Je lacht maar ik maak hier geen motherfokking grappen
 Pus uit m'n pik je kan een lauw biertje tappen
 Staat te spacen, maar ik ben niet van Star Trek
-Ook geen bhustabust maar ik breek wel je nek *ehehehehehe*
+Ook geen busta bust maar ik breek wel je nek *ehehehehehe*
 Komt alleen uit m'n achterste
 Moeders met dochters ze verachten me
 Ben van baksteen, alsof je dat niet ruikt
@@ -43,7 +43,8 @@ Ik ben tuig, van de eerste klasse
 Je wast je handen al tijdens het plassen
 Kijk me aan, je weet precies hoe laat het is
 Baksteen, de rest is geschiedenis
-Wat is niau kauw watskeburt in de schuur
+Wat is niau krauw?
+Watskeburt in de schuur
 Is bij jouw alles rustig of gezeur met de buur?
 Zijn het biertjes voor gratis of gewoon pleurisduur
 Ben je 'bout it bout it of een treurig figuur
@@ -51,7 +52,7 @@ Voor alle vrouwen en chickies, van Joure tot Maastrikkie
 Heb die vrouwen in m'n dickies yo we houden het jiggy
 'Bout it bout it net als een gek met al dat goud in m'n mikkie
 Tw&eacute;&eacute; gezichten een formule als Lauda en Niki
-Watser, watser, watser, watser, watser met jou ouwe?
+Watser, watser, watser, watser, watser met jouw ouwe?
 Keeenker keeeenker je staat te spacen
 Serieus gek wat praat je met mij man keeeeenker
 Je praat maar je weet niet eens watser met jou
@@ -61,12 +62,12 @@ mensen zijn aan de crack tegenwoordig, serieus
 Ik liep laatst door het Centraal
 komt er een junkie naar me toe voor een euro
 *euro, keeenker een euro?*
-Ik zeg hier heb je twintig cent, zegt-ie nee hoef ik niet
+Ik zeg hier heb je twintig cent, zegt-ie nee hoef je niet
 Dus ik zeg ja ja is goed ik ken jou ik ken jou, je komt uit Noord
 - uhh ja ik kom uit Noord hoe weet je dat hoe weet je dat
 Zeg ik ja ik ken je moeder ik heb m'n krantenwijk voor d'r bezorgd
 - Uhh m'n moeder m'n moeder? Ik heb haar al 25 jaar niet gezien
-Dus ik zeg fok jou man ik verkoop d'r crack stomme slet
+Dus ik zeg fok jou man ik verkoop 'r crack stomme slet
 Keenker keenker keenker junkies op het Centraal
 Serieus, enigste vette plek van Centraal is de fucking Pizzahut
 En uhh en en en en en de Meeting Point maar keenker die is dicht
